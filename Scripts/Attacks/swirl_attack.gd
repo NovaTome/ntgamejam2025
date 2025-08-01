b0599e234e29203c8e086eb735314deb1c6bcd10
Darklit <0lies18pies@gmail.com>
2025-08-01 16:38:57 -0400

    Complete swirl attack

--- Scripts/Attacks/swirl_attack.gd
+++ Scripts/Attacks/swirl_attack.gd
@@ -1,10 +1,9 @@
 extends Node2D
 class_name SwirlAttack
 
-@onready var path_follow_2d: PathFollow2D = $Path2D/PathFollow2D
-@onready var bullet_source: BulletSource = $Path2D/PathFollow2D/BulletSource
-
+@onready var bullet_source: BulletSource = $BulletSource
 
 func _on_timer_timeout() -> void:
 	bullet_source.fire()
-	path_follow_2d.progress_ratio+=0.01
+	bullet_source.global_rotation = wrapf(bullet_source.global_rotation-PI/16,PI/2,3*PI/2)
+	# I would like to make this so that it goes back and forth instead of snapping back to the top
