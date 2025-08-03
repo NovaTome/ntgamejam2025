5ec8d5825ed025c7fdbb9003b88755884ccdf1bc
henrycov <henrycov@gmail.com>
2025-08-03 11:24:42 -0400

    Add Winner Screen

--- Scripts/Autoloads/managers.gd
+++ Scripts/Autoloads/managers.gd
@@ -5,3 +5,7 @@ var self_management: SelfManagement
 var bullet_manager: BulletManager
 var sound_manager:SoundManager
 var map_manager:MainMap
+
+# These should go in another place, but we have a few hours left
+var game_won = false
+var game_won_ghosts = 60
--- Scripts/Entities/World/end_game_door.gd
+++ Scripts/Entities/World/end_game_door.gd
@@ -3,5 +3,7 @@ class_name Door
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		Managers.game_won = true
+		Managers.game_won_ghosts = GameConstants.STARTING_MAX_GHOSTS - Managers.self_management.ghostsRemaining
 		var mainGame:MainGame = Managers.map_manager.get_parent()
 		mainGame.animation_player.play("end")
