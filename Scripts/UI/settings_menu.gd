cc4a4c5c0f5831db25a457595bec36038990cb1c
Connor Markwell <markwell.connor@gmail.com>
2025-08-02 23:41:39 -0400

    Add settings menu

---
+++ Scripts/UI/settings_menu.gd
@@ -0,0 +1,21 @@
+extends VBoxContainer
+
+signal toggle_arachnophobia_mode
+signal music_volume_changed
+
+@onready var volume_slider = $VolumeHBoxContainer/HSlider
+@onready var volume_spin_box = $VolumeHBoxContainer/SpinBox
+
+@onready var check_button_status_label = $ArachnophobiaModeHBoxContainer/CheckButtonStatusLabel
+
+func _on_h_slider_value_changed(value):
+	volume_spin_box.set_value_no_signal(value)
+	music_volume_changed.emit(value)
+
+func _on_spin_box_value_changed(value):
+	volume_slider.set_value_no_signal(value)
+	music_volume_changed.emit(value)
+
+func _on_check_button_toggled(toggled_on):
+	check_button_status_label.text = "ON" if toggled_on else "OFF"
+	toggle_arachnophobia_mode.emit(toggled_on)
